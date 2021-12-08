396109b96fa9002e73711562823e9cc815745255
pamonha04 <93403951+pamonha04@users.noreply.github.com>
2021-12-08 13:29:43 -0300

    Add files via upload

---
+++ CordovaIMC/README.md
@@ -0,0 +1,7 @@
+** IMC **
+------------
+1 - npm install
+------------
+2 - cordova platform add browser
+------------
+3 - cordova run browser
---
+++ CordovaIMC/www/js/firebase.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7Qk8kHHfEgZ-i3VgErIWAbBjR8iI9Zxg",
+  authDomain: "rec-paloma.firebaseapp.com",
+  projectId: "rec-paloma",
+  storageBucket: "rec-paloma.appspot.com",
+  messagingSenderId: "973937592881",
+  appId: "1:973937592881:web:e1d3e3cf2f8c8077292310",
+  measurementId: "${config.measurementId}"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
